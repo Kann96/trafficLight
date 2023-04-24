f8a2d94106882738055b37a53332bb1f5e260d25
Kann96 <kann9620@gmail.com>
2023-04-24 17:16:12 +0300

    animation

--- trafficLight/ViewController.swift
+++ trafficLight/ViewController.swift
@@ -7,13 +7,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    
+    @IBOutlet var redView: UIView!
+    @IBOutlet var yellowView: UIView!
+    @IBOutlet var greenView: UIView!
+    @IBOutlet var startButton: UIButton!
+    
+    private var cornerRadius = 83.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        startButton.layer.cornerRadius = 10
+        redView.layer.cornerRadius = cornerRadius
+        yellowView.layer.cornerRadius = cornerRadius
+        greenView.layer.cornerRadius = cornerRadius
     }
-
-
+    
+    @IBAction func startButtonTapped() {
+        if startButton.titleLabel?.text == "Start"{
+            startButton.setTitle("Next", for: .normal)
+            redView.alpha = 1
+        } else if redView.alpha == 1 {
+            redView.alpha = 0.3
+            yellowView.alpha = 1
+        } else if yellowView.alpha == 1 {
+            yellowView.alpha = 0.3
+            greenView.alpha = 1
+        } else if greenView.alpha == 1 {
+            greenView.alpha = 0.3
+            redView.alpha = 1
+        }
+    }
 }
-
